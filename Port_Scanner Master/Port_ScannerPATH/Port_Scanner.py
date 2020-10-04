5390a845f083b3b660a453960c1b94c669a70843
VesuvianHector <66819590+VesuvianHector@users.noreply.github.com>
2020-10-03 23:16:25 -0400

    Update Port_Scanner.py

--- Port_Scanner Master/Port_ScannerPATH/Port_Scanner.py
+++ Port_Scanner Master/Port_ScannerPATH/Port_Scanner.py
@@ -12,20 +12,54 @@ import sys
 from colored import fg, bg, attr
 from datetime import datetime
 
-
+#####################################
+#DEFINITIONS AND UNIVERSAL VARIABLES#
+#####################################
 #Banner&Color
 Banner = fg('#00FF00') + bg('#808080') + attr(5)
 color  = fg('#FF0000') + bg('#808080')
+op     = fg('#00FF00') + bg('#000000') + attr(5)
+closed = fg('#FF0000') + bg('#000000')
 reset = attr(25)
 
 #BANNER
-print (Banner)
-banner = '/home/YOURUSERNAME/Port_ScannerPATH/Port_Scanner Banner.txt'
-f  = open(banner,"r")
-ascii = "".join(f.readlines())
-print(Banner + ascii)
-print("BY:JTH & Vesuvian Hector")
-print (reset)
+def BANNER():
+    print (Banner)
+    banner = '/home/vesuvian/Port_ScannerPATH/Port_Scanner Banner.txt'
+    f  = open(banner,"r")
+    ascii = "".join(f.readlines())
+    print(Banner + ascii)
+    print("BY:JTH & Vesuvian Hector")
+    print (reset)
+
+
+
+#Port Scanner Chunk
+def PortScanner(remoteServerIP, Low_port, High_port):
+    try:
+        for port in range(int(Low_port),int(High_port)):  #65535
+           s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            r = s.connect_ex((remoteServerIP, port))
+            if r == 0: 
+                print (op + "Port {}:        Open! :)".format(port) + reset)
+            else:
+                print (closed + "Port {}:        Closed :(".format(port))
+                
+        print (reset)       
+        s.close()
+
+    except KeyboardInterrupt:
+        print ("Exiting Ctrl+C")
+        sys.exit()
+
+    except socket.gaierror:
+        print ('Hostname of target could not be resolved. restarting')
+
+    except socket.error:
+        print ("Couldn't connect to target. restarting") 
+#__________________________________________________________________________________#
+
+Banner()#Prints Banner
 
 # Ask for input
 print(color)
@@ -33,50 +67,18 @@ print("_"*60)
 print ("Please provide the following information")
 remoteServer    = input("Enter a target to scan: ")
 remoteServerIP  = socket.gethostbyname(remoteServer)
-Low_port  = input("Enter lowest port:" )
-High_port = input("Enter highest port:" )
-
-    
+Low_port        = input("Enter lowest port:" )
+High_port       = input("Enter highest port:" )
 print ("-" * 60)
 print ("Please wait, scanning target", remoteServerIP)
 print ("-" * 60)
-
-# Check what time the scan started
-time1 = datetime.now()
+time1 = datetime.now()# Check what time the scan started
 print (time1)
 
-#Port Scanner Chunk
-try:
-    
-    for port in range(int(Low_port),int(High_port)):  #65535
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        r = s.connect_ex((remoteServerIP, port))
-        if r == 0:
-            print ("Port {}: 	 Open".format(port))
-    
-        else:
-            print ("Port {}: 	 Closed".format(port))
-            
-        s.close()
+PortScanner(remoteServerIP, Low_port, High_port)#PortScanner
 
-except KeyboardInterrupt:
-    print ("You pressed Ctrl+C")
-    sys.exit()
-
-except socket.gaierror:
-    print ('Hostname of target could not be resolved. restarting')
-
-except socket.error:
-    print ("Couldn't connect to target. restarting") 
-#__________________________________________________________________________________#
-
-#Timer ends
+  #Timer ends      
 time2 = datetime.now()
-
 print (time2)                                                   
-
 total =  time2 - time1
-
 print ('Scanning Completed in: ', total)
-
-
